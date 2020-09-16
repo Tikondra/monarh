07969dad40df0ad67ddc82d138fa3349cb2af2e0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-16 20:08:32 +0000

    Deploy Tikondra/monarh to github.com/Tikondra/monarh.git:gh-pages

---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function () {
+  var heightScroll = 180;
+
+  if (window.matchMedia("screen and (min-width:1004px)").matches) {
+    heightScroll = 10;
+  }
+
+  $("a.scroll-to").on("click", function(e){
+    e.preventDefault();
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(anchor).offset().top - heightScroll
+    }, 800);
+    var elements = document.querySelectorAll('.menu__link');
+
+    if (elements) {
+      for ( var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('menu__link--active');
+      }
+    }
+    $(this).addClass('menu__link--active');
+  });
+})();
+
+function modalDocView () {
+  var previewDoc = document.querySelectorAll('.licenses__doc img');
+  var modalDoc = document.querySelector('.licenses__modal');
+  var modalImg = modalDoc.querySelector('img');
+  var modalClose = modalDoc.querySelector('.licenses__modal-close');
+
+  previewDoc.forEach(function (item) {
+    item.addEventListener('click', function (evt) {
+      modalDoc.classList.add('licenses__modal--show');
+      modalImg.src = evt.target.dataset.src;
+      modalClose.addEventListener('click', function () {
+        modalDoc.classList.remove('licenses__modal--show');
+      })
+    })
+  })
+};
+
+if (document.querySelector('.licenses__modal')) {
+  modalDocView();
+};
